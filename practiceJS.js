5bd13ac5c0231a39b311a0a40a644e60e9220b8f
Alan Tan <alan@Alans-MacBook-Pro-2.local>
2023-01-04 19:03:52 -0800

    made a practice js file to practice 3js

---
+++ practiceJS.js
@@ -0,0 +1,11 @@
+import './style.css'; 
+
+import * as THREEJS from 'three'; 
+
+const scene = new THREEJS.Scene();
+const perspective_camera = new THREEJS.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 60, 10000)
+
+
+
+
+
